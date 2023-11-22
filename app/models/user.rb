be033e9ee1f906a917433f5b85b99bbe19f58c1f
Frcomparan <frcomparan@gmail.com>
2023-11-22 09:34:56 -0600

    Create simple CRUD for UUsers and add documentation for models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,15 @@
-class Category < ApplicationRecord
-    # Description validations 
+class User < ApplicationRecord
+    # Email validations
+    # - Should be uniquiq
+    # - Should have a valid format
     # - Should be present
-    validates :name, :description, presence: true
+    validates :email, uniqueness: true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
 
     # Name validations 
     # - Should be present
     # - Should't have numbers
-    validates :name, uniqueness: true
+    validates :name, :email, presence: true
     validates :name, format: { with: /\A[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ\-\. ]+\Z/,
     message: "The name doesn't have a valid format, it cannot have numbers" }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_22_150128) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_22_152123) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
